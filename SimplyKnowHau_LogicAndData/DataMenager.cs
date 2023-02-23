8874df4e7a987ec163d7a5def6648f7883069b3f
kUcLis <ulis89@gmail.com>
2023-02-23 14:57:11 +0100

    Added species json, added Species in DataMenager

--- SimplyKnowHau_LogicAndData/DataMenager.cs
+++ SimplyKnowHau_LogicAndData/DataMenager.cs
@@ -14,6 +14,7 @@ namespace SimplyKnowHau_LogicAndData
         public static List<Animal> Animals { get; set; } = GetList<Animal>("Animals.json");
         public static List<User> Users { get; set; } = GetList<User>("Users.json");
         public static List<Appointment> Appointments { get; set; } = GetList<Appointment>("Appointments.json");
+        public static List<Species> Species { get; set; } = GetList<Species>("Species.json");
 
         public static List<T> GetList<T>(string fileName)
         {
